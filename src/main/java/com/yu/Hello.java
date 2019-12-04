a0461bacf987809338e0a980e93d0544cb88441f
fionisme <36434845+fionisme@users.noreply.github.com>
2019-12-04 17:50:28 +0800

    Java and Kotlin var, data type

--- src/main/java/com/yu/Hello.java
+++ src/main/java/com/yu/Hello.java
@@ -5,5 +5,17 @@ public class Hello {
 //      System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 23;
+        Integer age2 =23;
+        char c = '我';
+        Character c2 = 'C'
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        //float height = 1.7f;
+        boolean adult = turn;
+        boolean enroll = false;
+        String name = "shan";
+
     }
 }
